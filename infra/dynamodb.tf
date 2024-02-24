1d7e87d248d09a3e3128d3a7d930bd515f019797
harrytjbreen <harrytjbreen@gmail.com>
2024-02-24 16:02:15 +0000

    feat: fixed db table, removed one for now due to a bug

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,23 +1,36 @@
-resource "aws_dynamodb_table" "map_last_played" {
-  name           = "map_last_played"
-  billing_mode   = "PAY_PER_REQUEST"
+# Due to bugs with CS2 we cannot get this information right now
+# Will be added in the future
 
-  attribute {
-    name = "id" // SteamID
-    type = "S"
-  }
-
-  dynamic "attribute" {
-    for_each = var.cs2_maps
-    content {
-      name = attribute.value
-      type = "S"
-    }
-  }
-
-  hash_key = "id"
-
-}
+#resource "aws_dynamodb_table" "map_last_played" {
+#  name           = "map_last_played"
+#  billing_mode   = "PAY_PER_REQUEST"
+#
+#  attribute {
+#    name = "id" // SteamID
+#    type = "S"
+#  }
+#
+#  attribute {
+#    name = "match-code"
+#    type = "S"
+#  }
+#
+#  attribute {
+#    name = "auth-token"
+#    type = "S"
+#  }
+#
+#  dynamic "attribute" {
+#    for_each = var.cs2_maps
+#    content {
+#      name = attribute.value
+#      type = "S"
+#    }
+#  }
+#
+#  hash_key = "id"
+#
+#}
 
 resource "aws_dynamodb_table" "report_list" {
   name = "report_list"
@@ -29,16 +42,6 @@ resource "aws_dynamodb_table" "report_list" {
   }
 
   attribute {
-    name = "banned"
-    type = "BOOL"
-  }
-
-  attribute {
-    name = "timestamp"
-    type = "N"
-  }
-
-  attribute {
     name = "steamId" //SteamID of the reported player
     type = "S"
   }
@@ -46,6 +49,18 @@ resource "aws_dynamodb_table" "report_list" {
   attribute {
     name = "reportedBy" //SteamID of the reporter
     type = "S"
+  }
+
+  global_secondary_index {
+    name = "SteamIdIndex"
+    hash_key = "steamId"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name = "SteamIdIndex"
+    hash_key = "reportedBy"
+    projection_type = "ALL"
   }
 
   hash_key = "id"
